24e75b0d54db21ed4b18507542f5774d78f0dadf
Sandeep Singh <contact.sandeep2004@gmail.com>
2017-01-17 13:33:40 -0500

    Add project files.

---
+++ FCClassManagementSystem/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(FCClassManagementSystem.Startup))]
+namespace FCClassManagementSystem
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
